739f35d539a79591fa5507404a1519baa72b3d82
xoxosmj <81194812+xoxosmj@users.noreply.github.com>
2024-10-28 17:14:48 +0900

    Title: 변수 출력하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241028/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = b;
+        String b = "C";
+        System.our.printf("%s!.....!%d",a,b);
+    }
+}
