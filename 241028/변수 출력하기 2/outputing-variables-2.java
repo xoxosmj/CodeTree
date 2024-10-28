869202d57718f5cb5dd3e8434ba22b214392788c
xoxosmj <81194812+xoxosmj@users.noreply.github.com>
2024-10-28 17:16:11 +0900

    Title: 변수 출력하기 2, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

--- 241028/변수 출력하기 2/outputing-variables-2.java
+++ 241028/변수 출력하기 2/outputing-variables-2.java
@@ -3,6 +3,6 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         int a = 3;
         String b = "C";
-        System.out.printf("%s!.....!%d",a,b);
+        System.out.printf("%s!.....!%d",b,a);
     }
 }
